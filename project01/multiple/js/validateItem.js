afdf02cac46dc5f51c10684d2a4427fbfe959c76
up1 <somkiat.p@gmail.com>
2023-04-17 15:28:03 +0700

    Working with response injection

--- project01/multiple/js/validateItem.js
+++ project01/multiple/js/validateItem.js
@@ -2,6 +2,7 @@ function (request) {
     if (request.path.indexOf('/api/v1/product/10') === 0) {
         return { statusCode: 500 };
     }
+
     return {
         statusCode: 200,
         headers: {
@@ -9,7 +10,8 @@ function (request) {
         },
         body: {
             "order_id": 8004359122,
-            "total_price": 14.95
+            "total_price": 14.95,
+            "timestamp": new Date().toString()
         }
       };
 }
